36f008ce00da769c5cff64b155db38a7b7a7358a
Nievas2 <angelgabrielnievas@gmail.com>
2025-04-16 13:47:18 -0300

    fix(ProjectsSection): :bug: Corregir descripciones y mejorar la estructura del componente

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -8,7 +8,7 @@ const ProjectsSection = () => {
       <h2 className="text-3xl md:text-4xl font-bold text-white px-4">
         Experiencia
       </h2>
-      <div className="grid md:grid-cols-2 gap-8 relative px-0 sm:px-6">
+      <div className="grid lg:grid-cols-2 gap-8 relative px-0 sm:px-6">
         {projects.map((project, index) => (
           <ProjectCard project={project} index={index} />
         ))}
@@ -26,71 +26,73 @@ function ProjectCard({ project, index }: { project: Project; index: number }) {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6, delay: index * 0.2 }}
       viewport={{ once: true }}
-      className="group"
+      className="group relative h-full"
     >
-      <div className="relative">
-        {/* Decorative elements */}
-        <div className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg blur-md opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
-        <div className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
+      {/* Decorative elements */}
+      <div className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg blur-md opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
+      <div className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-blue-400 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" />
+      {/* Content container */}
+      <div className="flex flex-col gap-2 relative p-3 sm:p-6 bg-[#12121a] rounded-lg h-full md:min-h-[630px]">
+        <img
+          src={project.src!}
+          alt={project.name}
+          className="h-[330px] rounded-xl"
+        />
 
-        {/* Content container */}
-        <div className="relative p-3 sm:p-6 bg-[#12121a] rounded-lg overflow-hidden min-w-fit md:min-h-[630px]">
-          <img
-            src={project.src!}
-            alt={project.name}
-            className="h-[100%] rounded-xl"
-          />
+        {/* Floating circles decoration */}
+        <div className="absolute top-0 right-0 -mr-8 -mt-8 w-32 h-32 bg-blue-600/10 rounded-full blur-2xl transform group-hover:translate-x-4 group-hover:-translate-y-4 transition-transform duration-700" />
+        <div className="absolute bottom-0 left-0 -ml-8 -mb-8 w-32 h-32 bg-blue-400/10 rounded-full blur-2xl transform group-hover:-translate-x-4 group-hover:translate-y-4 transition-transform duration-700" />
 
-          {/* Floating circles decoration */}
-          <div className="absolute top-0 right-0 -mr-8 -mt-8 w-32 h-32 bg-blue-600/10 rounded-full blur-2xl transform group-hover:translate-x-4 group-hover:-translate-y-4 transition-transform duration-700" />
-          <div className="absolute bottom-0 left-0 -ml-8 -mb-8 w-32 h-32 bg-blue-400/10 rounded-full blur-2xl transform group-hover:-translate-x-4 group-hover:translate-y-4 transition-transform duration-700" />
+        {/* Company badge */}
+        <div className="inline-flex items-center bg-blue-600/10 w-fit rounded-full px-3 py-1 text-sm text-blue-400">
+          <motion.span
+            initial={{ scale: 1 }}
+            whileHover={{ scale: 1.05 }}
+            viewport={{ once: true }}
+            className="relative z-10"
+          >
+            {project.role}
+          </motion.span>
+        </div>
 
-          {/* Company badge */}
-          <div className="inline-flex items-center bg-blue-600/10 rounded-full px-3 py-1 text-sm text-blue-400 my-4">
-            <motion.span
-              initial={{ scale: 1 }}
-              whileHover={{ scale: 1.05 }}
-              viewport={{ once: true }}
-              className="relative z-10"
-            >
-              {project.role}
-            </motion.span>
-          </div>
+        {/* Role and period */}
+        <div className="flex flex-col gap-3 h-full">
+          <h3 className="text-2xl font-bold text-blue-400 group-hover:text-blue-300 transition-colors">
+            {project.name}
+          </h3>
+          <p className="text-blue-200/80 text-sm">
+            {project.dateYear} | {project.dateI} | {project.dateF}
+          </p>
 
-          {/* Role and period */}
-          <div className="relative z-10">
-            <h3 className="text-2xl font-bold text-blue-400 mb-2 group-hover:text-blue-300 transition-colors">
-              {project.name}
-            </h3>
-            <p className="text-blue-200/80 text-sm mb-4">
-              {project.dateYear} | {project.dateI} | {project.dateF}
+          {/* Description with custom bullet points */}
+
+          <motion.div
+            initial={{ opacity: 0, x: -10 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.3, delay: 0.1 * index }}
+            viewport={{ once: true }}
+            className="flex items-start gap-3"
+          >
+            <p className="text-blue-100/80 group-hover:text-blue-100 transition-colors">
+              {project.description}
             </p>
+          </motion.div>
 
-            {/* Description with custom bullet points */}
-            <div className="space-y-3">
-              <motion.div
-                initial={{ opacity: 0, x: -10 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.3, delay: 0.1 * index }}
-                viewport={{ once: true }}
-                className="flex items-start gap-3"
-              >
-                <p className="text-blue-100/80 group-hover:text-blue-100 transition-colors">
-                  {project.description}
-                </p>
-              </motion.div>
-            </div>
-
-            {/* Technologies used */}
-            <div className="mt-6 flex flex-wrap gap-2">
+          {/* Technologies used */}
+          <div className="flex w-full h-full items-end">
+            <div className="flex flex-wrap gap-2">
               {project.tecnologies.map((tech, i) => (
-                <span
-                  key={i}
-                  className="flex items-center justify-center gap-2 px-2 py-1 text-xs bg-blue-600/10 text-blue-400 rounded-full group-hover:bg-blue-600/20 transition-colors"
-                >
-                  <img src={tech.src} alt={tech.name} height={16} width={16} />
-                  {tech.name}
-                </span>
+                <div key={i}>
+                  <span className="flex items-center justify-center gap-2 px-2 py-1 text-xs bg-blue-600/10 text-blue-400 rounded-full group-hover:bg-blue-600/20 transition-colors h-fit">
+                    <img
+                      src={tech.src}
+                      alt={tech.name}
+                      height={16}
+                      width={16}
+                    />
+                    {tech.name}
+                  </span>
+                </div>
               ))}
             </div>
           </div>
